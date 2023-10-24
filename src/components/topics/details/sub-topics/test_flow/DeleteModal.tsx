c8bf9dcc833abdf15364c76cd09d815ec2f17e54
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-10-24 15:36:28 +0100

    test flow for subtopics

---
+++ src/components/topics/details/sub-topics/test_flow/DeleteModal.tsx
@@ -0,0 +1,71 @@
+import { FormEvent, useState } from 'react';
+import CustomModal from '../../../../../common/CustomModal/CustomModal';
+import Button from '../../../../../common/Button';
+import { appAxios } from '../../../../../api/axios';
+import { sendCatchFeedback, sendFeedback } from '../../../../../functions/feedback';
+import { TestType } from '../../../../../types/data';
+import { CrossIcon } from '../../../../icons';
+
+interface Props {
+  closeModal: () => void;
+  open: boolean;
+  refetch: () => void;
+  data: TestType | undefined;
+}
+
+const deleteEvents = [
+  'Delete this test from its parent topic',
+  'Delete all the questions under this test',
+  'Clear all associated statistics',
+];
+
+function DeleteModal({ closeModal, refetch, open, data }: Props) {
+  const [loading, setLoading] = useState(false);
+
+  const submitValues = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await appAxios.delete('/tests/' + data?._id);
+      sendFeedback(response.data?.message, 'success');
+      refetch();
+      return closeModal();
+    } catch (error: any) {
+      sendCatchFeedback(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (!data) return null;
+  return (
+    <CustomModal isOpen={open} onRequestClose={closeModal} title='Delete Test'>
+      <form onSubmit={submitValues} className='w-full'>
+        <div className='w-full border-[0.6px] rounded-md border-[#DBDBDB] p-4 mt-7 mb-10'>
+          <h3 className='text-[#06102B] font-semibold text-lg mb-4 text-center'>
+            Deleting this test would do the following:
+          </h3>
+          <ul className='flex flex-col gap-3'>
+            {deleteEvents.map((item) => (
+              <li className='flex items-center gap-[10px] text-[#4B5768]' key={item}>
+                <CrossIcon />
+                <span>{item}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='flex items-center w-full justify-around gap-4 px-5'>
+          <Button type='submit' loading={loading} className='!w-full !bg-error'>
+            Delete
+          </Button>
+          <Button color='secondary' onClick={closeModal} className='!w-full'>
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </CustomModal>
+  );
+}
+
+export default DeleteModal;
--- src/pages/topics/subtopicDetails.tsx
+++ src/pages/topics/subtopicDetails.tsx
@@ -16,8 +16,9 @@ import SubTopicVideos from '../../components/topics/details/sub-topics/SubTopicV
 import AddFileModal from '../../components/topics/details/sub-topics/AddFileModal';
 import usePermissions from '../../hooks/usePermissions';
 import { PERMISSIONS } from '../../hooks/data';
+import TestList from '../../components/topics/details/sub-topics/test_flow/TestList';
 
-const tabs = ['About', 'Videos', 'Audios', 'Documents'];
+const tabs = ['About', 'Videos', 'Audios', 'Documents', 'Tests'];
 
 const SubTopicDetails = () => {
   const [data, setData] = useState<SingleSubTopicType | undefined>(undefined);
@@ -49,6 +50,7 @@ const SubTopicDetails = () => {
     <SubTopicVideos key='Videos' data={data} refetch={getData} />,
     <SubTopicAudios key='Audios' data={data} refetch={getData} />,
     <SubTopicDocuments key='Documents' data={data} refetch={getData} />,
+    <TestList key='Tests' subTopic={data?._id || ''} topic={data?.topic_id._id || ''} />,
   ];
 
   return (
