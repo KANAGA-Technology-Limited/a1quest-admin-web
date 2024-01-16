d3857d1c61a2aea70c09150e4f191c1c7386a1bd
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2024-01-16 16:53:48 +0100

    Update date.ts

--- src/functions/date.ts
+++ src/functions/date.ts
@@ -10,3 +10,16 @@ export const addOneDayToDate = (date: string) => {
     return newDate.toISOString().split('T')[0];
   }
 };
+
+export const addDayToDate = (date: string, numberOfDays: number = 1) => {
+  if (date) {
+    const newDate = new Date(date);
+    const currentDate = new Date();
+    newDate.setDate(newDate.getDate() + numberOfDays);
+    currentDate.setDate(currentDate.getDate() + numberOfDays);
+    if (newDate > currentDate) {
+      return currentDate.toISOString().split('T')[0];
+    }
+    return newDate.toISOString().split('T')[0];
+  }
+};
